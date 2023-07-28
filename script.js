f513afb4eb26bb7f46d7d9f928cf9764fedb71a1
frednguyen123 <fred.nguyen.2000@gmail.com>
2023-07-27 18:59:01 -0700

    Filter Pricing Section + Menu Backdrop + Responsive Media Queries

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ const bgImage = document.querySelector('.background__image')
 const initialCoords = header.getBoundingClientRect();
 console.log(initialCoords);
 
-
 window.addEventListener('scroll', function (e){
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
 
     if(window.scrollY > initialCoords.bottom) {
         nav.classList.add ('sticky');
@@ -19,6 +18,56 @@ window.addEventListener('scroll', function (e){
         bgImage.classList.remove('background__image-offscreen');
     }
 })
+
+filter('body');
+function filter(action){
+    let selected;
+    selected = document.getElementsByClassName("pricing__service");
+    let arr = [...selected];
+    let filteredArr = [];
+    let othersArr = [];
+    console.log(selected);
+    console.log(arr)
+
+    if(action == 'all'){
+        action == '';
+
+        arr.forEach(function(element, index, array){
+            element.classList += " active";
+        })
+    }
+    else{
+        arr.forEach(function(element, index, array){
+            if(element.classList.contains(action)){
+                filteredArr.push(element);
+            }
+            else{
+                othersArr.push(element);
+            }
+        })
+    
+        console.log(filteredArr);
+        console.log(othersArr);
+        
+        filteredArr.forEach(function(element, index, array){
+            element.classList += " active";
+        })
+    
+        othersArr.forEach(function(element, index, array){
+            element.classList.remove ("active");
+        })
+    
+        console.log(filteredArr);
+        console.log(othersArr);
+    }
+}
  
+function openMenu(){
+    document.body.classList += " menu--open"
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu--open')
+}
 
   
